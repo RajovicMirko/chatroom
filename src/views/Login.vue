5f8e7b2cd3b665772250bea3483e1814d39c8de7
Mirko Rajovic <Mirko.Rajovic@gemikro.rs>
2020-06-11 10:18:02 +0200

    created login form logic

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,80 @@
 <template>
-  <div>
-    <h1>This is {{this.$router.history.current.name}} page</h1>
+  <div class="login container d-flex justify-content-center align-items-center">
+    <form class="login-form" @submit="submitForm">
+      <div class="title text-center">
+        <h2>Chat login</h2>
+      </div>
+
+      <div class="inputs">
+        <input
+          ref="username"
+          type="username"
+          class="form-control"
+          id="username"
+          placeholder="Username"
+          v-model="loginData.username"
+        />
+        <input
+          type="password"
+          class="form-control"
+          id="password"
+          placeholder="Password"
+          v-model="loginData.password"
+        />
+      </div>
+
+      <div class="actions d-flex justify-content-between">
+        <button type="submit" class="btn btn-primary">Login</button>
+        <button type="button" class="btn btn-secondary" @click="regiserPage">Register</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      loginData: {
+        username: "",
+        password: ""
+      }
+    };
+  },
+
+  mounted() {
+    this.$refs.username.focus();
+  },
+
+  methods: {
+    submitForm(e) {
+      e.preventDefault();
+      console.log("submit login form");
+      console.log(this.loginData);
+    },
+
+    regiserPage() {
+      this.$router.push({
+        path: "/register"
+      });
+    }
+  }
 };
 </script>
 
 <style>
+.login {
+  /* Solve full height problem */
+  /* height: 100vh; */
+}
+
+.login-form {
+  width: 350px;
+}
+
+.login-form .inputs input {
+  margin-bottom: 1rem;
+  box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.15);
+}
 </style>
