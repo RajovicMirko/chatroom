664dacd7c561fbb378a059128f8feff8b3d6b650
Mirko Rajovic <Mirko.Rajovic@gemikro.rs>
2020-06-12 09:53:46 +0200

    Navigation bar added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/login">Login</router-link>|
-      <router-link to="/register">Register</router-link>|
-      <router-link to="/chat">Chat</router-link>
-    </div>
+    <Navigation />
     <router-view />
   </div>
 </template>
+
+<script>
+import Navigation from "./components/Navigation";
+export default {
+  name: "App",
+  components: { Navigation }
+};
+</script>
 
 <style>
 #app {
---
+++ src/components/Navigation.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Brand -->
+    <router-link class="navbar-brand" to="/">
+      <img src="../assets/chatLogo.svg" width="30" height="30" alt loading="lazy" />
+      ChatManiac
+    </router-link>
+
+    <!-- Mobile button -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active" v-for="route in routes" :key="route.path">
+          <router-link class="nav-link" :to="route.path">
+            {{route.name}}
+            <span class="sr-only">(current)</span>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      routes: this.$router.options.routes.filter(route => route.path !== "/")
+    };
+  }
+};
+</script>
+
+<style>
+</style>
