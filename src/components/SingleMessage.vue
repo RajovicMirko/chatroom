b50db774ef976a39f258c36a5cef3ee608a07b34
Mirko Rajovic <Mirko.Rajovic@gemikro.rs>
2020-06-18 12:33:25 +0200

    Chat page small fix

--- src/components/SingleMessage.vue
+++ src/components/SingleMessage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="msg" :class="msg.type">
+  <div id="msg" :class="msg.type" @click="msgClick">
     <small class="time-stamp">18:20</small>
     <li class="txt list-group-item list-group-item-action d-flex align-items-center">{{msg.text}}</li>
   </div>
@@ -11,6 +11,12 @@ export default {
     msg: {
       type: Object,
       required: true
+    }
+  },
+
+  methods: {
+    msgClick() {
+      console.log("Msg is clicked");
     }
   }
 };
@@ -43,11 +49,11 @@ export default {
 
 #msg.send {
   align-self: flex-end;
-  border: 1px solid rgba(128, 128, 128, 0.5);
+  border: 1px solid rgba(128, 128, 128, 0.05);
 }
 
 #msg.recived {
-  background-color: #007bff;
+  background-color: rgba(0, 123, 255, 1);
   color: rgb(255, 255, 255);
 }
 
